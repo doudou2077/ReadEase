718aab4842dc72763555da8805b373e47f1710b1
lqingman <qingmanli@gmail.com>
2025-01-26 23:38:21 -0800

    modify for settings

--- readease/public/background.js
+++ readease/public/background.js
@@ -1,8 +1,8 @@
 
-// Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
+// // Allows users to open the side panel by clicking on the action toolbar icon
+// chrome.sidePanel
+//   .setPanelBehavior({ openPanelOnActionClick: true })
+//   .catch((error) => console.error(error));
 
 chrome.action.onClicked.addListener((tab) => {
     // Inject the content script into the active tab
